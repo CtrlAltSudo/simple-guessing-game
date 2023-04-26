317de1b94d1f6d68dbf1825e7d15e7636234634a
Damien White <damienkwhite@gmail.com>
2023-04-26 20:30:15 +0100

    created starter variables

---
+++ app.js
@@ -0,0 +1,6 @@
+let welcome = alert("welcome to a simple guessing game!");
+let maxNum = prompt("Please enter a maximum number to guess");
+while(!maxNum){
+    maxNum = parseInt(prompt("Please enter a valid number"))
+}
+
