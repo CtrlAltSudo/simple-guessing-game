7f063d1355c747566c69951f9c6ca5d2f0b8b002
Damien White <damienkwhite@gmail.com>
2023-04-27 06:55:10 +0100

    added ability to quit

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 let welcome = alert("welcome to a simple guessing game!");
 let maxNum = prompt("Please enter a maximum number to guess");
-while(!maxNum){
-    maxNum = parseInt(prompt("Please enter a valid number"))
-}
+let guess = prompt("whats your first guess?");
+let attempts = 1;
+const targetNum = Math.floor(Math.random() * maxNum) +1;
+
+while (parseInt(guess) !== targetNum){
+    attempts ++;
+    if (guess === "q" || guess === "quit") break;
+     if (guess > targetNum) {
+       guess = prompt("Guess is too high, try again?");
+     } else {
+        guess = prompt("Guess is too low, try again?");
+     }
+
+}   
+if (guess === "q" || guess === "quit"){
+    alert("Ok, you quit.")
+} else alert(`You got it! It took you ${attempts} attempts to guess.`)
+location.reload(); 
 
